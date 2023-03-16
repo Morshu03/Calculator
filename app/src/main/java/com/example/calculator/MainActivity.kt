d3154d0090c7bfd3fa0ad426b762889b17c13a66
Morshu03 <varvaroom03@gmail.com>
2023-03-16 14:36:07 +0300

    activitymain gets a viewmodel; method calculate transferred to viewmodel; from activity called viewmodel's method;

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.example.calculator
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import com.example.calculator.databinding.ActivityMainBinding
 import com.example.calculator.model.CalculatorViewModel
-import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,46 +15,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val viewModel: CalculatorViewModel by viewModels()
 
         binding.decisionButton.setOnClickListener {
             val aValue = binding.editTextSeniorCoefficient.text.toString()
             val bValue = binding.editTextAverageCoefficient.text.toString()
             val cValue = binding.editTextFreeMember.text.toString()
+
             if (aValue.isNotEmpty() && bValue.isNotEmpty() && cValue.isNotEmpty()) {
-                calculate(
+                val result = viewModel.calculate(
                     aValue = aValue.toDouble(),
                     bValue = bValue.toDouble(),
                     cValue = cValue.toDouble()
                 )
+                if (result != null) {
+                    binding.xOne.text = result.first.toString()
+                    binding.xTwo.text = result.second.toString()
+                } else {
+                    Toast.makeText(
+                        this@MainActivity, "Дискриминант равен 0", Toast.LENGTH_SHORT
+                    ).show()
+                }
             } else {
                 Toast.makeText(
                     this@MainActivity, "Введены не все коофиценты", Toast.LENGTH_SHORT
                 ).show()
             }
-        }
-    }
-
-    @SuppressLint("SetTextI18n", "StringFormatMatches")
-    private fun calculate(
-        aValue: Double,
-        bValue: Double,
-        cValue: Double
-    ) {
-        val x1: Double
-        val x2: Double
-
-        val discriminant: Double = bValue * bValue - 4 * aValue * cValue
-
-        if (discriminant >= 0) {
-            x1 = (-bValue + sqrt(discriminant)) / (2 * aValue)
-            x2 = (-bValue - sqrt(discriminant)) / (2 * aValue)
-
-            binding.xOne.text = resources.getString(R.string.x1, x1)
-            binding.xTwo.text = resources.getString(R.string.x2, x2) //вынестив ресурсы
-
-        } else if (discriminant < 0) {
-            binding.discriminantLessZero.text =
-                resources.getString(R.string.discriminant, discriminant)//вынестив ресурсы
         }
     }
 }
